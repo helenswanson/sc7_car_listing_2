8e8d091397b45f06ce2f536b21dba987d9f459be
helenswanson <hhyswanson@gmail.com>
2014-07-01 07:14:43 -0400

    Second spec (incomplete form submission) passes for user adding a new manufacturer.

---
+++ app/controllers/manufacturers_controller.rb
@@ -0,0 +1,32 @@
+class ManufacturersController < ApplicationController
+  def index
+    @manufacturers = Manufacturer.all
+  end
+
+  def show
+    @manufacturer = Manufacturer.find(params[:id])
+    @car = Car.new
+    @cars = Car.all
+  end
+
+  def new
+    @manufacturer = Manufacturer.new
+  end
+
+  def create
+    @manufacturer = Manufacturer.new(manufacturer_params)
+
+    if @manufacturer.save
+      flash[:notice] = "Success"
+      redirect_to manufacturer_path(manufacturer)
+    else
+      flash.now[:notice] = "Error"
+      render :new
+    end
+  end
+
+  private
+  def manufacturer_params
+    params.require(:manufacturer).permit(:name, :country)
+  end
+end
---
+++ spec/support/factories.rb
@@ -0,0 +1,16 @@
+FactoryGirl.define do
+  factory :manufacturer do
+    name 'Honda'
+    country 'Japan'
+  end
+
+  # factory :car do
+  #   make 'Civic'
+  #   manufacturer 'Honda'
+  #   color 'silver'
+  #   mileage 75000
+  #   year 2001
+  #   description 'This is a car.'
+  # end
+end
+
